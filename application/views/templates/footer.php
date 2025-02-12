1bcbd58ed58bd86e4c8d837aa114a5495b53737c
Joken <Joken@GAMDIAS.com>
2025-02-12 13:35:22 +0800

    initial commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,14 @@
+<footer class="main-footer">
+  <div class="pull-right hidden-xs">
+  </div>
+  <strong> &copy; 2020 - <?php echo date('Y'); ?> - Parking Management System
+</footer>
+
+<!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+<div class="control-sidebar-bg"></div>
+<!-- ./wrapper -->
+
+</body>
+
+</html>
